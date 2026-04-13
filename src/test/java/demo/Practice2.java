0d5bfeab548f5e200c0b4a63c22da1d47621da70
KARTHI <KARTHI@karthikk>
2026-04-13 09:50:36 +0530

    This is my forth commite

--- src/test/java/demo/Practice2.java
+++ src/test/java/demo/Practice2.java
@@ -1,10 +1,10 @@
 package demo;
 
-public class Practice2 {
-
+public class Practice2 
+{
 	public static void main(String[] args) 
 	{
 		System.out.println("Nikhil is bad boy");
+		System.out.println("Nikhil committed");
 	}
-
 }
